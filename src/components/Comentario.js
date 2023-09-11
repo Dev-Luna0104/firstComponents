f53ca7e5e83a6390cccb9b67897c307198e2ba8d
Dev-Luna0104 <matheuslunacamargo@gmail.com>
2023-09-11 09:40:01 -0300

    Construinda componentes

---
+++ src/components/Comentario.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Comentario.css';
+
+const Comentario = (    ) =>(
+    <div className="Container-comentario">
+        <h2 className="C01">João:</h2>
+        <h3 className="C01">Olá, tudo bem?</h3>
+    </div>
+)
+
+export default Comentario 
+console.log(Comentario())
